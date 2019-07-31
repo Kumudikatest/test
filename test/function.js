17a5084a2241581c45b5b272ee95716618888fb5
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-31 18:13:40 +0530

    test

--- test/function.js
+++ test/function.js
@@ -1,4 +1,17 @@
-exports.handler = function(request, response) {
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (request, response) {
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_D10y3fy0o",
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
     //
-    response.send({"message": "Successfully executed"});
+    response.send({ "message": "Successfully executed" });
 }
