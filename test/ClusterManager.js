d10e1790ab68f772fa453d326223838639947618
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 11:03:25 +0530

    test

---
+++ test/ClusterManager.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+    this.clusters = [];
+
+    this.clusters["k1234567890123456789"] = {
+        host: "k1234567890123456789.y6jiqb.clustercfg.use1.cache.amazonaws.com",
+        port: 6379,
+        clusterModeEnabled: true
+    };
+};
--- test/function.js
+++ test/function.js
@@ -1,16 +1,22 @@
-let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
+let SL_REDIS = require('slappforge-sdk-redis');
+let clusterManager = require('./ClusterManager');
+const redis = new SL_REDIS.Redis(clusterManager);
 
 exports.handler = function (request, response) {
-    kinesis.describeStream({
-        StreamName: 'testindunil'
-    }).promise()
-        .then(data => {
-            // your logic goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    // You must always quit the redis client after it's used
+    redis.sinterstore({
+        clusterIdentifier: 'k1234567890123456789',
+        params: [{
+            key: '2',
+            values: ['2']
+        }]
+    }, function (error, response, redisClient) {
+        if (error) {
+            callback(error);
+        } else {
+            redisClient.quit();
+        }
+    });
 
     response.send({ "message": "Successfully executed" });
 }
