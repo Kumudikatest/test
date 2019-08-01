71811bf07520a786a3321ebd6432834e1b7f3c14
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 09:57:16 +0530

    test

--- test/function.js
+++ test/function.js
@@ -1,18 +1,29 @@
 let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (request, response) {
-
-    cognito_idp.listUsers({
-        UserPoolId: "us-east-1_D10y3fy0o",
-        Limit: 10
-    }, function (error, data) {
-        if (error) {
-            // implement error handling logic here
-            throw error;
-        }
-        // your logic goes within this block
-    });
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
     response.send({ "message": "Successfully executed" });
 }
