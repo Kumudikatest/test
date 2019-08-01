a358fa92b790501bb288838ebe86b7cb4233db0b
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 10:02:49 +0530

    test

--- test/function.js
+++ test/function.js
@@ -1,25 +1,12 @@
 let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-const sqs = new SL_AWS.SQS(AWS);
+const sns = new AWS.SNS();
 
 exports.handler = function (request, response) {
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '1',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '0'
+    sns.getTopicAttributes({
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                // No messages to process
-            }
+        .then(data => {
+            // your code goes here
         })
         .catch(err => {
             // error handling goes here
