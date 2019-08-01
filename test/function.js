2ce8726d89f38eedad620a1ac83705d8a6f84960
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 10:12:01 +0530

    test

--- test/function.js
+++ test/function.js
@@ -1,28 +1,36 @@
 let AWS = require('aws-sdk');
-const ses = new AWS.SES();
+const s3 = new AWS.S3();
 
 exports.handler = function (request, response) {
-    ses.sendEmail({
-        Destination: {
-            ToAddresses: ['kumudika@adroitlogic.com'],
-            CcAddresses: [],
-            BccAddresses: []
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: ``
-                }
-            },
-            Subject: {
-                Data: 'Test'
+    s3.listObjects({
+        'Bucket': 'as2-test-lahiru',
+        'MaxKeys': 10,
+        'Prefix': ''
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                Contents: [
+                    {
+                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                       Key: "example1.jpg",
+                       LastModified: "<Date Representation>",
+                       Owner: {
+                          DisplayName: "myname",
+                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                       },
+                       Size: 11,
+                       StorageClass: "STANDARD"
+                    },
+                    // {...}
+                ]
             }
-        },
-        Source: 'kumudika@adroitlogic.com',
-    }, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-    });
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
     response.send({ "message": "Successfully executed" });
 }
