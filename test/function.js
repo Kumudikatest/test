84da6c5d5b4ebcf49a52ae89fba2aaeecb425056
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-01 10:06:22 +0530

    test

--- test/function.js
+++ test/function.js
@@ -1,16 +1,28 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+const ses = new AWS.SES();
 
 exports.handler = function (request, response) {
-    sns.getTopicAttributes({
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['kumudika@adroitlogic.com'],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: ``
+                }
+            },
+            Subject: {
+                Data: 'Test'
+            }
+        },
+        Source: 'kumudika@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
     response.send({ "message": "Successfully executed" });
 }
