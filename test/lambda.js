21a407f8cf9debbb53d77cca566d389e86c4d44a
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-02 15:14:50 +0530

    test

--- test/lambda.js
+++ test/lambda.js
@@ -10,11 +10,29 @@ exports.handler = function (event, context, callback) {
             // implement error handling logic here
             console.log(error);
             throw error;
-        } else { 
-           console.log(data);
-       }
+        } else {
+            console.log(data);
+        }
         // your logic goes within this block
     });
+    cognito_idp.adminCreateUser({
+        UserPoolId: process.env.UserPoolId_cognitoUnnamed,
+        Username: "Test",
+        DesiredDeliveryMediums: ["SMS"],
+        ForceAliasCreation: false,
+        TemporaryPassword: "Test#123",
+        UserAttributes: [],
+        ValidationData: []
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        } else {
+            console.log(data);
+        }
+        // your logic goes within this block
+    });
+
 
     callback(null, { "message": "Successfully executed" });
 }
