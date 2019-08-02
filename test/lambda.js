9ca54e0b551c091e67e17efdbf8302d2e6a4f889
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-02 14:58:28 +0530

    test

--- test/lambda.js
+++ test/lambda.js
@@ -1,4 +1,20 @@
-exports.handler = function(event, context, callback) {
-    //
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitoUnnamed,
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            console.log(error);
+            throw error;
+        } else { 
+           console.log(data);
+       }
+        // your logic goes within this block
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
