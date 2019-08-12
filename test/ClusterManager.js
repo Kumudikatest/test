66303a06dfcf3f8eaaf7d6c85f793704e8bd5f8e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-12 15:45:36 +0530

    test

--- test/ClusterManager.js
+++ test/ClusterManager.js
@@ -1,9 +1,9 @@
 module.exports = function() {
     this.clusters = [];
 
-    this.clusters["k1234567890123456789"] = {
-        host: "k1234567890123456789.y6jiqb.clustercfg.use1.cache.amazonaws.com",
+    this.clusters["k"] = {
+        host: process.env.EndPoint_redisK,
         port: 6379,
-        clusterModeEnabled: true
+        clusterModeEnabled: false
     };
 };
--- test/lambda.js
+++ test/lambda.js
@@ -1,4 +1,26 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let SL_REDIS = require('slappforge-sdk-redis');
+let clusterManager = require('./ClusterManager');
+const redis = new SL_REDIS.Redis(clusterManager);
+
+exports.handler = function (event, context, callback) {
+    // You must always quit the redis client after it's used
+    redis.set({
+        clusterIdentifier: 'k',
+        params: [{
+            key: 'FName',
+            value: 'kumu'
+        }, {
+            key: 'LName',
+            value: 'rupe'
+        }]
+    }, function (error, response, redisClient) {
+        if (error) {
+            callback(error);
+        } else {
+            callback(response);
+            redisClient.quit();
+        }
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
