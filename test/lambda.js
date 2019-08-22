08f4444406d47ddb3b6543298c9581c445bc852e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-22 16:40:48 +0530

    test

--- test/lambda.js
+++ test/lambda.js
@@ -1,4 +1,29 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+
+exports.handler = function (event, context, callback) {
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/anomalyDetectionQueue`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
