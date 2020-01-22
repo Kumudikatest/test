537cacf51644b3c9a8ce1b9851a6f90be7155101
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-01-22 13:14:43 +0530

    test

--- test/lambda.js
+++ test/lambda.js
@@ -1,16 +1,4 @@
-let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-
 exports.handler = async (event) => {
-    ddb.scan({
-        TableName: "new"
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
