a5ae755d99d985a4a014b77720167eed4f92ccf4
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-02-03 15:56:54 +0530

    cog in cali

--- test/lambda.js
+++ test/lambda.js
@@ -1,4 +1,17 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitonew,
+        Limit: 10
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    return { "message": "Successfully executed" };
 };
