a6caa0304f214ecdf7162065c31b847a3df2aee0
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-02-27 10:33:19 +0530

    test

--- lambda.js
+++ lambda.js
@@ -1,19 +1,28 @@
 let AWS = require('aws-sdk');
-const translate = new AWS.Translate();
+const ses = new AWS.SES();
 
 exports.handler = async (event) => {
-    translate.translateText({
-        SourceLanguageCode: "auto",
-        TargetLanguageCode: "zh",
-        Text: "Tianjin"
-    }).promise()
-        .then(data => {
-            // your code goes here
-            console.log(data);
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['kumudika@adroitlogic.com'],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: ``
+                }
+            },
+            Subject: {
+                Data: 'Test'
+            }
+        },
+        Source: 'kumudika+syd@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
     return { "message": "Successfully executed" };
 };
