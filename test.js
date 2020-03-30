f520d6092d35303ff3423dc9f695eef04c2cf610
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-03-30 17:16:08 +0530

    test

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
-exports.handler = function(request, response) {
-    
-    response.send({"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const eventBridge = new AWS.EventBridge();
+
+exports.handler = function (request, response) {
+    eventBridge.putEvents({
+        Entries: [{
+            EventBusName: "my-event-bus-udith",
+            Source: "bv",
+            DetailType: " nvn",
+            Detail: "{}"
+        }]
+    }).promise()
+        .then(data => {
+            console.loh(data);
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    response.send({ "message": "Successfully executed" });
 }
