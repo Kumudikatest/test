7b95c80ab854e6ceb21ea151c89611e7d1772438
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-03-31 18:42:22 +0530

    test

--- test.js
+++ test.js
@@ -1,19 +1,4 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-var common = require('./common');
-
 exports.handler = async (event) => {
-    //common.func1();
-    console.log(common);
-    try {
-        let data = await cognito_idp.listUsers({
-            UserPoolId: process.env.UserPoolId_cognitol,
-            Limit: 10
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
