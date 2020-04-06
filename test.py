0db73c4a79fcea890d52d82d01b296faafdbf3bf
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-06 15:50:58 +0530

    test

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-import boto3
-ses = boto3.client("ses")
-
 def handler(event, context):
-    try:
-        data = ses.send_email(
-            Source="kumudika@adroitlogic.com",
-            Destination={
-                'ToAddresses': ["hirudinee@adroitlogic.com"],
-                'CcAddresses': ["hirudinee+aws@adroitlogic.com"],
-                'BccAddresses': ["indunil@adroitlogic.com"]
-            },
-            Message={
-                'Subject': {
-                    'Data': "Please Ignore This Test Message"
-                },
-                'Body': {
-                    'Text': {
-                        'Data': ""
-                    }
-                }
-            }
-        )
-    except BaseException as e:
-        print(e)
-        raise(e)
+   
     
     return {"message": "Successfully executed"}
