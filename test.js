bf692e0dc736f0bfd3d041445897e02d904d2726
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-06 16:05:03 +0530

    test

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-
-exports.handler = async (event) => {
-    try {
-        let data = await sns.listSubscriptionsByTopic({
-            TopicArn: "arn:aws:sns:us-east-1:318300609668:New"
-        }).promise();
-    console.log(data);
-    } catch (err) {
-        // error handling goes here
-    };
-
-    return { "message": "Successfully executed" };
-};
+exports.handler = function(request, response) {
+    
+    response.send({"message": "Successfully executed"});
+}
