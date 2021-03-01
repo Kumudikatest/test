c6908f7cc03018d7139e220e30663bad99701f8f
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-03-01 17:06:05 +0530

    API and Cog

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
+const AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    debugger;
+    try {
+        let data = await cognito_idp.listUsers({
+            UserPoolId: "us-east-1_HdYJb7Znp",
+            Limit: 10
+        }).promise();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
 };
